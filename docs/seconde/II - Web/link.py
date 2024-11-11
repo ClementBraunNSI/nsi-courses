50c64aec74df33e38b35572ad79e42854902bf2f
OuraClem <clement.braun1@outlook.fr>
2024-11-11 21:49:01 +0100

    images

---
+++ docs/seconde/II - Web/link.py
@@ -0,0 +1,18 @@
+import os
+
+def images_par_classes():
+    with open('liens.txt', 'w') as f:
+        f.write('')
+    for classe in os.listdir(('./monstres')):
+            print(classe)
+            with open('liens.txt', 'a') as f:
+                 f.write(f"## {classe}\n")
+            if classe != '.DS_Store':
+                for image in os.listdir('./monstres/' + classe):
+                    print(image)
+                    if image != '.DS_Store':
+                        if ' ' in image:
+                            os.rename('./monstres/' + classe + '/' + image, './monstres/' + classe + '/' + image.replace(' ', '_'))
+                        with open('liens.txt', 'a') as f:
+                            f.write(f"![{classe}_{image[0:len(image)-4]}]("+'monstres/' + classe + '/' + image + ')\n')
+images_par_classes()
