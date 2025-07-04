99e1a2bbf7313bfc999532bc31a9c3e2f07958fa
OuraClem <clement.braun1@outlook.fr>
2025-07-04 14:54:11 +0200

    kljkl

--- site_final/public/courses-markdown/premiere/Aides/Revisions/exos_dictionnaires.md
+++
@@ -1,3 +0,0 @@
-# Révisions dictionnaires
-
-TODO  
--- site_final/public/courses-markdown/premiere/XIII-Projets/Gestion_Jeux_Video/correction.py
+++
@@ -1,7 +0,0 @@
-ludotheque = {}
-
-def ajouter_jeu(ludotheque:list, titre : str, plateforme:str, annee:int, genre:str,statut:str):
-    for jeu in ludotheque:
-        if jeu["titre"] == titre:
-            return False
-        
